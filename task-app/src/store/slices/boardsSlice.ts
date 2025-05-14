5a52eb83570e215ed8cebc48aa2949df966ce864
MinWu CHO <mauercho@gmail.com>
2025-05-15 00:01:50 +0900

    EditModal 컴포넌트 생성

--- task-app/src/store/slices/boardsSlice.ts
+++ task-app/src/store/slices/boardsSlice.ts
@@ -26,6 +26,12 @@ type TAddTaskAction = {
   task: ITask;
 };
 
+type TDeleteTaskAction = {
+  boardId: string;
+  listId: string;
+  taskId: string;
+};
+
 const initialState: TBoardsState = {
   modalActive: false,
   boardArray: [
@@ -96,6 +102,49 @@ const boardsSlice = createSlice({
           : board
       );
     },
+
+    updateTask: (state, { payload }: PayloadAction<TAddTaskAction>) => {
+      state.boardArray = state.boardArray.map((board) =>
+        board.boardId === payload.boardId
+          ? {
+              ...board,
+              lists: board.lists.map((list) =>
+                list.listId === payload.listId
+                  ? {
+                      ...list,
+                      tasks: list.tasks.map((task) =>
+                        task.taskId === payload.task.taskId
+                          ? payload.task
+                          : task
+                      ),
+                    }
+                  : list
+              ),
+            }
+          : board
+      );
+    },
+
+    deleteTask: (state, { payload }: PayloadAction<TDeleteTaskAction>) => {
+      state.boardArray = state.boardArray.map((board) =>
+        board.boardId === payload.boardId
+          ? {
+              ...board,
+              lists: board.lists.map((list) =>
+                list.listId === payload.listId
+                  ? {
+                      ...list,
+                      tasks: list.tasks.filter(
+                        (task) => task.taskId !== payload.taskId
+                      ),
+                    }
+                  : list
+              ),
+            }
+          : board
+      );
+    },
+
     deleteList: (state, { payload }: PayloadAction<TDeleteListAction>) => {
       state.boardArray = state.boardArray.map((board) =>
         board.boardId === payload.boardId
@@ -113,6 +162,13 @@ const boardsSlice = createSlice({
     },
   },
 });
-export const { addBoard, deleteList, setModalActive, addTask, addList } =
-  boardsSlice.actions;
+export const {
+  addBoard,
+  deleteList,
+  setModalActive,
+  addTask,
+  addList,
+  deleteTask,
+  updateTask,
+} = boardsSlice.actions;
 export const boardsReducer = boardsSlice.reducer;
