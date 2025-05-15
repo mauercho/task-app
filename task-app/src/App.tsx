efe4bf72bc38cabf2fbd5f7fad78c30612bf4047
MinWu CHO <mauercho@gmail.com>
2025-05-16 01:13:42 +0900

    task들 drag and drop 기능 추가

--- task-app/src/App.tsx
+++ task-app/src/App.tsx
@@ -12,9 +12,10 @@ import ListsContainer from "./components/ListsContainer/ListsContainer";
 import { useTypedDispatch, useTypedSelector } from "./hooks/redux";
 import EditModal from "./components/EditModal/EditModal";
 import LoggerModal from "./components/LoggerModal/LoggerModal";
-import { deleteBoard } from "./store/slices/boardsSlice";
+import { deleteBoard, sort } from "./store/slices/boardsSlice";
 import { v4 } from "uuid";
 import { addLog } from "./store/slices/loggerSlice";
+import { DragDropContext } from "@hello-pangea/dnd";
 
 function App() {
   const [isLoggerOpen, setIsLoggerOpen] = useState(false);
@@ -52,6 +53,45 @@ function App() {
       alert("최소 게시판 개수는 한 개입니다.");
     }
   };
+
+  const handleDragEnd = (result: any) => {
+    const { destination, source, draggableId } = result;
+    const sourceList = lists.filter(
+      (list) => list.listId === source.droppableId
+    )[0];
+    console.log(sourceList);
+    dispatch(
+      sort({
+        boardIndex: boards.findIndex(
+          (board) => board.boardId === activeBoardId
+        ),
+        droppableIdStart: source.droppableId,
+        droppableIdEnd: destination.droppableId,
+        droppableIndexStart: source.index,
+        droppableIndexEnd: destination.index,
+        draggableId,
+      })
+    );
+
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `리스트 "${sourceList.listName}"에서
+          리스트 "${
+            lists.filter((list) => list.listId === destination.droppableId)[0]
+              .listName
+          }"으로
+          ${
+            sourceList.tasks.filter((task) => task.taskId === draggableId)[0]
+              .taskName
+          }을 옮김.
+          `,
+        logAuthor: "User",
+        logTimestamp: String(Date.now()),
+      })
+    );
+  };
+
   return (
     <div className={appContainer}>
       {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
@@ -61,7 +101,9 @@ function App() {
         setActiveBoardId={setActiveBoardId}
       />
       <div className={board}>
-        <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
+        </DragDropContext>
       </div>
       <div className={buttons}>
         <button className={deleteBoardButton} onClick={handleDeleteBoard}>
--- task-app/src/store/slices/boardsSlice.ts
+++ task-app/src/store/slices/boardsSlice.ts
@@ -36,6 +36,15 @@ type TDeleteBoardAction = {
   boardId: string;
 };
 
+type TSortAction = {
+  boardIndex: number;
+  droppableIdStart: string;
+  droppableIdEnd: string;
+  droppableIndexStart: number;
+  droppableIndexEnd: number;
+  draggableId: string;
+};
+
 const initialState: TBoardsState = {
   modalActive: false,
   boardArray: [
@@ -170,6 +179,30 @@ const boardsSlice = createSlice({
     setModalActive: (state, { payload }: PayloadAction<boolean>) => {
       state.modalActive = payload;
     },
+
+    sort: (state, { payload }: PayloadAction<TSortAction>) => {
+      // same list
+      if (payload.droppableIdStart === payload.droppableIdEnd) {
+        const list = state.boardArray[payload.boardIndex].lists.find(
+          (list) => list.listId === payload.droppableIdStart
+        );
+
+        // 변경시키는 아이템을 배열에서 지워줌
+        // return 값으로 지워진 아이템을 잡아줌줌
+        const card = list?.tasks.splice(payload.droppableIndexStart, 1);
+        list?.tasks.splice(payload.droppableIndexEnd, 0, ...card!);
+      }
+      if (payload.droppableIdStart !== payload.droppableIdEnd) {
+        const listStart = state.boardArray[payload.boardIndex].lists.find(
+          (list) => list.listId === payload.droppableIdStart
+        );
+        const card = listStart!.tasks.splice(payload.droppableIndexStart, 1);
+        const listEnd = state.boardArray[payload.boardIndex].lists.find(
+          (list) => list.listId === payload.droppableIdEnd
+        );
+        listEnd?.tasks.splice(payload.droppableIndexEnd, 0, ...card);
+      }
+    },
   },
 });
 export const {
@@ -181,5 +214,6 @@ export const {
   deleteTask,
   updateTask,
   deleteBoard,
+  sort,
 } = boardsSlice.actions;
 export const boardsReducer = boardsSlice.reducer;
